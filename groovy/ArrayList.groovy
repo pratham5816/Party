8b39c2036722a7cdb335822d9323c84f2183c602
pratham kumar <prathamtaheem@gmail.com>
2026-04-13 19:50:49 +0530

    sql | groovy

---
+++ groovy/ArrayList.groovy
@@ -0,0 +1,18 @@
+
+first = ["hello" , "world" , "xyz"]
+
+println first.join(" - ")
+println first.getClass()
+
+println Math.max(10,200)
+
+for(int i = 0; i < 3; i++){
+    System.out.println("Ho");
+}
+
+System.out.println("Merry");
+
+
+0.upto(5){
+    println "heloo"
+}
